0a805916b88831fd58d76a7a437a9731612b43fc
narad <jason.narad@gmail.com>
2016-07-21 18:28:03 +0100

    fixes to squad converter

--- quebap/io/squad2quebap.py
+++ quebap/io/squad2quebap.py
@@ -2,14 +2,13 @@ import json
 
 
 def convert_squad(file_path):
-    print(file_path)
     question_sets = []
     with open(file_path) as data_file:
         data = json.load(data_file)['data']
         for article in data:
             for paragraph in article['paragraphs']:
                 qa_set = {
-                    'support': parse_support(paragraph),
+                    'support': [parse_support(paragraph)],
                     'questions': [parse_question(qa_dict) for qa_dict in paragraph['qas']]
                 }
                 question_sets.append(qa_set)
@@ -17,7 +16,9 @@ def convert_squad(file_path):
 
 def parse_support(para_dict):
     return {
-        'src': para_dict['context']
+        'document': {
+            'text': para_dict['context']
+        }
     }
 
 def parse_question(qa_dict):
@@ -29,14 +30,15 @@ def parse_question(qa_dict):
     chosen_answer_start = chosen_answer['answer_start']
     result_answer_dict = {
         'text': chosen_answer_text,
-        'span': {
-            'start': chosen_answer_start,
-            'end': chosen_answer_start + len(chosen_answer_text)
-        }
+        'span': []
+#{
+#            'start': chosen_answer_start,
+#            'end': chosen_answer_start + len(chosen_answer_text)
+#        }
     }
     return {
         'question': qa_dict['question'],
-        'answer': result_answer_dict
+        'answers': [result_answer_dict]
     }
 
 # Usage:
@@ -45,7 +47,6 @@ def parse_question(qa_dict):
 # From command line, two arguments converts arg1 and writes to arg2
 def main():
     import sys
-    print(sys.argv[1])
     corpus = convert_squad(sys.argv[1])
     if len(sys.argv) == 2:
         print(json.dumps(corpus, indent=2))
