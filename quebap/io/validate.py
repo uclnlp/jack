33aafe5a76676524221c919d920c27ff165cc236
JohannesMaxWel <johannes.welbl.14@ucl.ac.uk>
2016-07-21 16:27:34 +0100

    added preliminary dataset schema, minor change to validation script.

--- quebap/io/validate.py
+++ quebap/io/validate.py
@@ -2,16 +2,15 @@ import json
 import jsonschema
 from sys import argv
 
-schema = open(argv[1]).read() 
-data = open(argv[2]).read()
+schema = eval(open(argv[1]).read())
+data = eval(open(argv[2]).read())
 
 try:
 	jdata = json.loads(data)
 	jschema = json.loads(schema)
 	jsonschema.validate(jdata, jschema)
+	print('JSON successfully validated.')
 except jsonschema.ValidationError as e:
 	print(e.message)
 except jsonschema.SchemaError as e:
 	print(e)
-
-print('JSON successfully validated.')
