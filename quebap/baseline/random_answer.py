15f01f222aa32eee390d5cfbe7c632d18a250fb2
narad <jason.narad@gmail.com>
2016-07-22 07:21:17 +0100

    added random answer baseline

---
+++ quebap/baseline/random_answer.py
@@ -0,0 +1,26 @@
+import json
+import random
+import sys
+
+
+def get_answers(data):
+    deep_answers = [get_answers_to_qset(qset) for qset in data]
+    flat_answers = [item for sublist in deep_answers for item in sublist]
+    return flat_answers
+
+def get_answers_to_qset(qset):
+    return [[random.choice(q['candidates'])] for q in qset['questions']]
+
+def read_data(data_filename):
+    with open(data_filename) as data_file:
+        data = json.load(data_file)
+        return data
+
+def main():
+    random.seed(1)
+    if len(sys.argv) == 2:
+        data = read_data(sys.argv[1])
+        answers = get_answers(data)
+        print(json.dumps(answers, indent=2))
+
+if __name__ == "__main__": main()
