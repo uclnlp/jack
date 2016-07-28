813d1fe4af61aa7aa1bf6624aa4af56dd7a97d27
mbosnjak <matko.bosnjak@gmail.com>
2016-07-28 14:11:15 +0100

    Closed #20

---
+++ quebap/util/glove.py
@@ -0,0 +1,51 @@
+from quebap.util.vocabulary import Vocabulary
+import numpy as np
+
+
+def load_glove(stream, vocab=None):
+    print('[Loading GloVe]')
+    word2idx = {}
+
+    first_line = stream.readline()
+    dim = len(first_line.split()) - 1
+
+    lookup = np.empty([500000, dim], dtype=np.float)
+    lookup[0] = np.fromstring(first_line.split(maxsplit=1)[1], sep=' ')
+    word2idx[first_line.split(maxsplit=1)[0]] = 0
+
+    n = 1
+    idx = 1
+    for line in stream:
+        word, vec = line.rstrip().split(maxsplit=1)
+        if vocab is None or word in vocab:
+            word = word.decode('utf-8')
+            word2idx[word] = idx
+            if idx > np.size(lookup, axis=0) - 1:
+                lookup.resize([lookup.shape[0] + 500000, lookup.shape[1]])
+            lookup[idx] = np.fromstring(vec, sep=' ')
+            idx += 1
+        n += 1
+        if n % 100000 == 0:
+            print('  ' + str(n // 1000) + 'k vectors processed...\r')
+
+    lookup.resize([idx, dim])
+    return_vocab = Vocabulary(word2idx)
+    print('[Loading GloVe DONE]')
+
+    return return_vocab, lookup
+
+
+if __name__ == "__main__":
+    pickle_tokens = False
+
+    import zipfile
+
+    with zipfile.ZipFile('../data/GloVe/glove.840B.300d.zip') as zf:
+        with zf.open('glove.840B.300d.txt', 'r') as f:
+            vocab, lookup = load_glove(f)
+
+            # pickle token set
+            if pickle_tokens:
+                import pickle
+                glove_words = set(vocab.get_all_words())
+                pickle.dump(glove_words, open('./data/glove_tokens.pickle', 'wb'))
---
+++ quebap/util/vocabulary.py
@@ -0,0 +1,73 @@
+import itertools
+
+
+class Vocabulary:
+    def __init__(self, vocab=None):
+        self.__word2idx = {}
+        self.__freqs = {}
+        if vocab is not None and isinstance(vocab, dict):
+            self.__word2idx = {key: value for key, value in vocab.items()}
+            self.__freqs = {key: None for key, _ in vocab.items()}
+        self.__idx2word = None
+
+    def __str__(self):
+        return 'Vocabulary size: ' + str(self.get_size()) + \
+               ' First 5 elements: ' + str(list(itertools.islice(self.__word2idx.keys(), 0, 5)))
+
+    def add_iterable(self, itr):
+        for item in itr:
+            yield self.add_word(item)
+
+    def add_word(self, word):
+        if word not in self.__word2idx:
+            self.__word2idx[word] = len(self.__word2idx)
+            self.__freqs[word] = 1
+            self.__idx2word = None
+            return len(self.__word2idx) - 1
+        else:
+            self.__freqs[word] += 1
+            return self.__word2idx[word]
+
+    def get_idx_by_word(self, word):
+        return self.__word2idx.get(word, None)
+
+    def get_word_count(self, word):
+        return self.__freqs.get(word, None)
+
+    def get_size(self):
+        return len(self.__word2idx)
+
+    def __len__(self):
+        return len(self.__word2idx)
+
+    def get_all_words(self):
+        return self.__word2idx.keys()
+
+    @property
+    def word2idx(self):
+        return self.__word2idx
+
+    @property
+    def idx2word(self):
+        if self.__idx2word is None:
+            self.__idx2word = {idx: word for word, idx in self.__word2idx.items()}
+        return self.__idx2word
+
+    def get_word_by_idx(self, idx):
+        return self.idx2word.get(idx, None)
+
+    def dump_all_tokens_to_file(self, filename):
+        with open(filename, 'w') as f:
+            for token in self.__word2idx.keys():
+                f.write(token + '\n')
+
+    def contains_word(self, word):
+        return word in self.__word2idx
+
+    def diff(self, other):
+        if not isinstance(other, Vocabulary):
+            raise TypeError()
+        return set(self.__word2idx.keys()).difference(set(other.get_all_words()))
+
+    def sorted(self):
+        return sorted(self.__word2idx.items(), key=lambda x: -x[1][1])
