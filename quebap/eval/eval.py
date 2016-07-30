29b3faccc935bbe36e1a324c92ea4031fe95b4ef
narad <jason.narad@gmail.com>
2016-07-30 22:15:32 +0100

    added mean reciprocal rank but haven't tested yet, waiting on models producing rank outpud

--- quebap/eval/eval.py
+++ quebap/eval/eval.py
@@ -1,7 +1,7 @@
 import sys
 import json
 
-def score(predictions, golds, k=1):
+def hits_at_k(predictions, golds, k=1):
     gold_answers = get_gold_answers(golds)
     assert len(predictions) == len(gold_answers)
     num_pred_answers = 0
@@ -16,8 +16,26 @@ def score(predictions, golds, k=1):
                 if top_k_answer['text'] == gold_answer['text']:
                     hits += 1
                     break
-    print("Accuracy: ({0}/{1}) = {2:3.3f}".format(hits, num_pred_answers, hits * 100.0 / num_pred_answers))
+    return hits * 100.0 / num_pred_answers
+#    print("Accuracy: ({0}/{1}) = {2:3.3f}".format(hits, num_pred_answers, ))
 
+# untested (no models currently produce rank output)
+def mean_reciprocal_rank(predictions, golds):
+    gold_answers = get_gold_answers(golds)
+    assert len(predictions) == len(gold_answers)
+    rank_sum = 0
+    # pred and gold are lists of answers for the same question
+    for pred,gold in zip(predictions, gold_answers):
+        best_rank = 0
+        # assumes pred predictions are in ranked order
+        # could also sort by 'score' field if found
+        for rank,value in enumerate(pred):
+            for gold_value in gold:
+                if value == gold_value and rank < best_rank:
+                    best_rank = rank
+                    break
+        rank_sum += 1/(best_rank+1)
+    return rank_sum / len(predictions)
 
 def get_gold_answers(golds):
     return [question['answers']
@@ -37,6 +55,9 @@ def main():
     if len(sys.argv) == 3:
         predictions = read_data(sys.argv[1])
         gold = read_data(sys.argv[2])
-        score(predictions, gold)
+        score = hits_at_k(predictions, gold, k=1)
+        print('Accuracy: {0:3.3f}'.format(score))
+        mrr = mean_reciprocal_rank(predictions, gold)
+        print('Mean Reciprocal Rank: {0:3.3f}'.format(mrr))
 
 if __name__ == "__main__": main()
