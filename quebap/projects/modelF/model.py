ee5c305cdbe1248029eccfdc21892db2dbc73118
JohannesMaxWel <johannes.welbl.14@ucl.ac.uk>
2016-08-11 16:18:26 +0100

    negative log likelihood loss for true and false facts

--- quebap/projects/modelF/model.py
+++ quebap/projects/modelF/model.py
@@ -5,8 +5,8 @@ from util import *
 
 def define_model_F_score(tuples, entity_pair_embeddings, relation_embeddings):
     '''
-    Returns dot products of relation embedding and ent. pair embedding, batchwise.
-    tuples is a matrix  [batchsize x 2].
+    Returns dot products of relation embedding and entity pair embedding,
+    batchwise. 'tuples' is a matrix: [batchsize x 2].
     Each row contains an integer index for the relation, one for entity pair.
     entity_pair_embeddings: [N_entity_pairs x k]
     relation_embeddings: [N_relations x k]
@@ -27,16 +27,35 @@ def define_model_F_score(tuples, entity_pair_embeddings, relation_embeddings):
     return dot_products
 
 
+def nll_loss(dot_products_true, dot_products_false, normalize=True):
+    '''
+    Given a set of dot product scores for positive facts [batchsize x 1]
+    and a set of dot product scores for negative facts [batchsize x 1]
+    compute negative log-likelihood loss and return single scalar value.
+    Normalization divides loss by batchsize.
+    '''
+    true_contribution = tf.nn.softplus(-dot_products_true)
+    false_contribution = tf.nn.softplus(dot_products_false)
+    if normalize:
+        total_batch_loss = tf.reduce_mean(true_contribution \
+                                            + false_contribution)/2.0
+    else:
+        total_batch_loss = tf.reduce_sum(true_contribution + false_contribution)
+    return total_batch_loss
+
+
 def test():
     k = 5
     batchsize = 17
     n = 10
-    tuples = tf.Variable(np.random.randint(0, n, [batchsize, 2]))
+    tuples_true = tf.Variable(np.random.randint(0, n, [batchsize, 2]))
+    tuples_false = tf.Variable(np.random.randint(0, n, [batchsize, 2]))
     E2_emb = tf.Variable(np.random.normal(0.0, 1.0, [n, k]))
     R_emb = tf.Variable(np.random.normal(0.0, 1.0, [n, k]))
-    dot_products = define_model_F_score(tuples, E2_emb, R_emb)
-    tfrunprint(dot_products)
-
+    dot_products_true = define_model_F_score(tuples_true, E2_emb, R_emb)
+    dot_products_false = define_model_F_score(tuples_false, E2_emb, R_emb)
+    loss = nll_loss(dot_products_true, dot_products_false)
+    tfrunprint(loss)
 
 if __name__ == "__main__":
     test()
