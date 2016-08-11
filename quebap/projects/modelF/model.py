8e11be19b22f894206d20bac3d74ad671510a981
JohannesMaxWel <johannes.welbl.14@ucl.ac.uk>
2016-08-11 17:54:33 +0100

    added (generic?) trainer

--- quebap/projects/modelF/model.py
+++ quebap/projects/modelF/model.py
@@ -1,8 +1,13 @@
 import tensorflow as tf
+from tensorflow import ConfigProto
+
 import numpy as np
+from numpy.random import randint
 from util import *
 from copy import copy
+from time import time
 
+SEED = 42
 
 def define_model_F_score(tuples, entity_pair_embeddings, relation_embeddings):
     '''
@@ -56,12 +61,12 @@ def batcher(batchsize, training_data, negative_domain_size):
     '''
     ### create positive training instances
     n = training_data.shape[0]
-    random_example_indices = np.random.randint(low=0, high=n, size=batchsize)
+    random_example_indices = randint(low=0, high=n, size=batchsize)
     true_training_instances = training_data[random_example_indices,:]
 
     ### create negative training instances
     # pick random indices (of entity pairs)
-    random_negative_indices = np.random.randint(0, negative_domain_size,
+    random_negative_indices = randint(0, negative_domain_size,
                                                 size=batchsize)
     # copy the true training instances,
     false_training_instances = copy(true_training_instances)
@@ -82,21 +87,79 @@ def batcher(batchsize, training_data, negative_domain_size):
     return facts_shuffled, gold_shuffled
 
 
+def initialize_embeddings(n_entity_pairs, n_relations, k, sigma=0.1):
+    '''
+    Initialise Gaussian embeddings for entity pairs and relations with sigma
+    the standard deviation and k the latent dimensionality.
+    '''
+    entity_pair_embeddings = tf.Variable(tf.random_normal([n_entity_pairs, k], \
+                                         0.0, sigma, seed=randint(SEED)))
+
+    relation_embeddings = tf.Variable(tf.random_normal([n_relations, k], \
+                                      0.0, sigma, seed=randint(SEED)))
+    return entity_pair_embeddings, relation_embeddings
+
+
+def training(loss, batch_function, placeholders, data, batchsize=32,
+             learning_rate=0.1, optimization='Adam', n_iterations=20):
+    ''' Training procedure for optimising a predefined loss over data using a
+    batch_function to create training instances that are fed into the input
+    placeholders.
+    '''
+    n_entity_pairs = 3
+    # define optimizer
+    if optimization == 'SGD':
+        print("using SGD trainer")
+        SGD = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+        update_op = SGD.minimize(tf.reduce_mean(loss))
+    else:
+        print("using Adam trainer")
+        Adam = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        update_op = Adam.minimize(tf.reduce_mean(loss))
+
+    # Pontus' advice on efficiency in parallel...
+    config = ConfigProto(
+            inter_op_parallelism_threads=1,
+            intra_op_parallelism_threads=1,
+            )
+    with tf.Session(config=config) as sess:
+        sess.run(tf.initialize_all_variables())
+        t0 = time()
+        for iteration in range(0, n_iterations):
+            minibatch_facts, minibatch_gold = batch_function(batchsize, data)
+            feed = {placeholders["facts"]: minibatch_facts,
+                    placeholders["gold"]: minibatch_gold
+                    }
+            _, loss__ = sess.run((update_op, loss), feed_dict=feed)
+            print(iteration, loss__)
+    return
+
+
+
 def test():
     k = 5
     batchsize = 3
-    n = 10
-    E2_emb = tf.Variable(np.random.normal(0.0, 1.0, [n, k]))
-    R_emb = tf.Variable(np.random.normal(0.0, 1.0, [n, k]))
-    data = np.random.randint(0, n, [10, 2])
+    n_data = 7
+    n_entity_pairs = 13
+    n_relations = 17
+    E2_emb, R_emb = initialize_embeddings(n_entity_pairs, n_relations, k)
+
+    placeholders = {"facts": tf.placeholder(dtype=tf.int64, \
+                                                shape=(None,2), name='facts'),
+                    "gold": tf.placeholder(dtype=tf.int64, \
+                                                shape=None, name='gold')
+                    }
+    data = randint(0, n_data, [10, 2])
     facts, gold = batcher(batchsize, data, 10)
     print('data', data)
     print('true and false facts shuffled', facts)
     print('gold shuffled', gold)
+
     dot_product_scores = define_model_F_score(facts, E2_emb, R_emb)
     loss = nll_loss(dot_product_scores, gold, normalize=True)
-    tfrunprint(loss)
-
+    # specify number of entity pairs for batcher
+    batch_function = (lambda bs, d: batcher(bs, d, n_entity_pairs))
+    training(loss, batch_function, placeholders, data)
 
 
 if __name__ == "__main__":
