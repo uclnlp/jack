80f8d4a4a88f4ec964502f83eeede00d6b3b0938
rockt <tim.rocktaeschel@gmail.com>
2016-09-02 12:27:23 +0100

    mapping from quebap source vocab to autoreader target vocab

--- quebap/projects/autoread/eval.py
+++ quebap/projects/autoread/eval.py
@@ -13,21 +13,42 @@ def load_vocab(path, max_vocab_size=50000):
             vocab[splits[1]] = int(splits[0])
     return vocab
 
+
+def vocab_to_ixmap(vocab):
+    ixmap = {}
+    for word in vocab:
+        ixmap[vocab[word]] = word
+    return ixmap
+
+
+def reindex_seq(seq, source_vocab_ixmap, target_vocab):
+    """
+    :param seq: [batch_size x max_seq_length] int32 word ids
+    :param source_vocab_ixmap: dictionary mapping source word ids to words
+    :param target_vocab: dictionary mapping targit words to ids
+    :return: [batch_size x max_seq_length] int32 with target word ids
+    """
+    for row in range(len(seq)):
+        for col in range(len(seq[0])):
+            word = source_vocab_ixmap[seq[row][col]]
+            if word not in target_vocab:
+                word = "<UNK>"
+
+            seq[row][col] = target_vocab[word]
+
+
 if __name__ == '__main__':
     with open("./quebap/data/LS/debug/lexsub_debug_cleaned.jsonl", "r") as f:
         data = json.load(f)
     tensorizer = GenericTensorizer(data)
 
     quebap_vocab = tensorizer.question_lexicon
+    quebap_vocab_ixmap = vocab_to_ixmap(quebap_vocab)
     vocab = load_vocab("./quebap/projects/autoread/document.vocab")
-    # todo map from quebap to autoreader vocabs
-
-    print(quebap_vocab)
-    print(vocab)
 
     config = {
-        "size": 3, # todo
-        "vocab_size": 5, # todo
+        "size": 3,  # todo
+        "vocab_size": len(vocab),
         "is_train": False,
     }
 
@@ -47,12 +68,13 @@ if __name__ == '__main__':
         sess.run(tf.initialize_all_variables())
 
         for quebap_batch in tensorizer.create_batches(data, batch_size=batch_size):
-            batch = [
-                quebap_batch[tensorizer.questions],
-                quebap_batch[tensorizer.question_lengths],
-                np.ones((batch_size,
-                         len(quebap_batch[tensorizer.questions][0])))
-            ]
+            # todo: reindex words
+            seq = quebap_batch[tensorizer.questions]
+            reindex_seq(seq, quebap_vocab_ixmap, vocab)
+
+            batch = [seq, quebap_batch[tensorizer.question_lengths],
+                     np.ones((batch_size,
+                              len(quebap_batch[tensorizer.questions][0])))]
 
             results = reader.run(sess, [outputs], batch)
 
