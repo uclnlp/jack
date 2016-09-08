fb2d8a90a01982070927c7e96d9dd51e9b9b0ab1
JohannesMaxWel <johannes.welbl.14@ucl.ac.uk>
2016-09-08 15:16:38 +0100

    fixing import error in cbt test

--- quebap/io/test_cbt.py
+++ quebap/io/test_cbt.py
@@ -1,5 +1,8 @@
-import validate
+import quebap.io.validate as validate
 
 def test_cbt_snippet_format():
-    response = validate.main('../data/snippet/CBT/cbt_NE_train_snippet.quebap.json', 'dataset_schema.json')
+    response = validate.main('quebap/data/snippet/CBT/cbt_NE_train_snippet.quebap.json', 'quebap/io/dataset_schema.json')
     assert response == 'JSON successfully validated.'
+
+
+test_cbt_snippet_format()
