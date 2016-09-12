1ae4f7c9701e386bcc51554d9c94f2b9b7c8ba72
Thomas Demeester <thomas.demeester@ugent.be>
2016-09-12 22:58:48 +0100

    building blocks for baseline model

--- quebap/projects/suppoRTE/prepare_embeddings.py
+++ quebap/projects/suppoRTE/prepare_embeddings.py
@@ -10,21 +10,21 @@ def _prepare_embeddings(fname, format, kwargs={}):
     :param format: word2vec_binary, glove 
     :param kwargs: needed in case format=='glove'; dict with keys 'dim' and 'vocab_size'
     """
-    fname_out = '.'.join(fname.split('.')[:-1]) + '.pickle'
-    emb = we.load_embedding(fname, format, normalize=False, lower=False, clean_words=False, load_kwargs=kwargs)    
+    fname_out = '.'.join(fname.split('.')[:-1]) + '.pkl'
+    emb = we.load_embedding(fname, format, normalize=False, lower=True, clean_words=False, load_kwargs=kwargs)    
     pickle.dump(emb,open(fname_out,'wb'))
     print('wrote embeddings to ',fname_out)
     return emb
 
-def load(fname, format='pickle', kwargs={}):
+def load(fname, format='pkl', kwargs={}):
     """
     load embeddings from prepared pickle file, or from original file
     :param fname: filename
-    :param format: pickle, word2vec_binary, glove
+    :param format: pkl | word2vec_binary | glove
     :param kwargs: needed in case format=='glove'; dict with keys 'dim' and 'vocab_size'
     """
-    if format=='pickle':
-        print('load pickle file')
+    if format=='pkl':
+        #print('load pickle file %s'%fname)
         return pickle.load(open(fname,'rb'))
     else:
         return _prepare_embeddings(fname, format, kwargs)
@@ -33,7 +33,7 @@ def load(fname, format='pickle', kwargs={}):
 
 if __name__ == '__main__':
     #emb = load(path.join('quebap','data','GloVe','glove.6B.50d.txt'), 'glove', {'vocab_size':400000,'dim':50})
-    emb = load(path.join('quebap','data','GloVe','glove.6B.50d.pickle'))
+    emb = load(path.join('quebap','data','GloVe','glove.6B.50d.pkl'))
     print(emb.shape)
     print(emb.get('hello'))
     
