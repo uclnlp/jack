a4244e2452b9dba32a985ec053175e19161f2ff7
rockt <tim.rocktaeschel@gmail.com>
2016-10-27 11:41:16 +0100

    pre and post run hooks into training loop

--- quebap/projects/sisyphos/train.py
+++ quebap/projects/sisyphos/train.py
@@ -1,11 +1,8 @@
 import tensorflow as tf
 
 
-#train(loss, optim, train_feed_dicts, max_epochs=1000, hooks=hooks)
-
-
 def train(loss, optim, batches, placeholders=None, predict=None, max_epochs=10,
-          hooks=[], sess=None):
+          hooks=[], pre_run=None, post_run=None, sess=None):
     min_op = optim.minimize(loss)
     if sess is None:
         sess = tf.Session()
@@ -14,15 +11,19 @@ def train(loss, optim, batches, placeholders=None, predict=None, max_epochs=10,
         tf.initialize_all_variables().run()
 
         for i in range(max_epochs):
-            for batch in batches:
-                if placeholders is None: #then batches should be feed_dict
-                    feed_dict = batch
+            for j, batch in enumerate(batches):
+                if placeholders is not None:
+                    feed_dict = dict(zip(placeholders, batch))
                 else:
-                    if isinstance(placeholders, dict) and isinstance(batch, dict):
-                        feed_dict = {placeholders[k]: batch[k] for k in placeholders}
-                    else:
-                        feed_dict = dict(zip(placeholders, batch))
+                    feed_dict = batch
+
+                if pre_run is not None:
+                    pre_run(sess, i+1, feed_dict, loss, predict)
+
                 _, current_loss = sess.run([min_op, loss], feed_dict=feed_dict)
+
+                if post_run is not None:
+                    post_run(sess, i+1, feed_dict, loss, predict)
 
                 for hook in hooks:
                     hook(sess, i+1, predict, current_loss)
