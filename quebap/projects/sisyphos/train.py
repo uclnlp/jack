2118e9622de1ea30eb96ef39492120be4514b957
rockt <tim.rocktaeschel@gmail.com>
2016-10-27 11:59:44 +0100

    keeping session of training loop alive

--- quebap/projects/sisyphos/train.py
+++ quebap/projects/sisyphos/train.py
@@ -7,27 +7,26 @@ def train(loss, optim, batches, placeholders=None, predict=None, max_epochs=10,
     if sess is None:
         sess = tf.Session()
 
-    with sess:
-        tf.initialize_all_variables().run()
+    tf.initialize_all_variables().run(session=sess)
 
-        for i in range(max_epochs):
-            for j, batch in enumerate(batches):
-                if placeholders is not None:
-                    feed_dict = dict(zip(placeholders, batch))
-                else:
-                    feed_dict = batch
+    for i in range(max_epochs):
+        for j, batch in enumerate(batches):
+            if placeholders is not None:
+                feed_dict = dict(zip(placeholders, batch))
+            else:
+                feed_dict = batch
 
-                if pre_run is not None:
-                    pre_run(sess, i+1, feed_dict, loss, predict)
+            if pre_run is not None:
+                pre_run(sess, i+1, feed_dict, loss, predict)
 
-                _, current_loss = sess.run([min_op, loss], feed_dict=feed_dict)
+            _, current_loss = sess.run([min_op, loss], feed_dict=feed_dict)
 
-                if post_run is not None:
-                    post_run(sess, i+1, feed_dict, loss, predict)
+            if post_run is not None:
+                post_run(sess, i+1, feed_dict, loss, predict)
 
-                for hook in hooks:
-                    hook(sess, i+1, predict, current_loss)
-
-            # calling post-epoch hooks
             for hook in hooks:
-                hook(sess, i+1, predict, 0)
+                hook(sess, i+1, predict, current_loss)
+
+        # calling post-epoch hooks
+        for hook in hooks:
+            hook(sess, i+1, predict, 0)
