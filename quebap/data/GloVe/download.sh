69f31fd50b028c03b3abd8ec975b2cbc9b35c06a
rockt <tim.rocktaeschel@gmail.com>
2016-12-01 20:14:24 +0000

    getting Matkos GloVe embeddings

--- quebap/data/GloVe/download.sh
+++ quebap/data/GloVe/download.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#wget http://nlp.stanford.edu/data/glove.6B.zip
-#unzip glove.6B.zip
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip
 #wget http://nlp.stanford.edu/data/glove.42B.300d.zip
 #unzip glove.42B.300d.zip
-wget http://nlp.stanford.edu/data/glove.840B.300d.zip
-unzip glove.840B.300d.zip
+#wget http://nlp.stanford.edu/data/glove.840B.300d.zip
+#unzip glove.840B.300d.zip
