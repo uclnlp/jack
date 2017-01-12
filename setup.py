995dda13d8397cee4231a79067fb932b8bccdd76
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-01-12 14:58:02 +0100

    setup.* files for turning quebap into a library

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='quebap',
+      version='0.1.0',
+      description='QUEstion answering and knowledge BAse Population',
+      author='UCL Machine Reading',
+      author_email='s.riedel@cs.ucl.ac.uk',
+      url='https://github.com/uclmr/quebap',
+      test_suite='tests',
+      license='MIT',
+      install_requires=[
+            'tensorflow>=0.8'
+      ],
+      packages=find_packages())
