ece023c0635e613dbb4b561e638aef0dd7e88ad9
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-01-12 20:35:09 +0100

    Updating outdated tests

--- quebap/load/test_cbt.py
+++
@@ -1,8 +0,0 @@
-import quebap.io.validate as validate
-
-def test_cbt_snippet_format():
-    response = validate.main('quebap/data/CBT/snippet_quebapformat.json', 'quebap/io/dataset_schema.json')
-    assert response == 'JSON successfully validated.'
-
-
-test_cbt_snippet_format()
