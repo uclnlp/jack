d822a3ad7542a03a517457452c46741b2fa68387
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-01-13 16:14:42 +0100

    PEP 8

--- quebap/baseline/random_answer.py
+++ quebap/baseline/random_answer.py
@@ -1,20 +1,26 @@
+# -*- coding: utf-8 -*-
+
 import json
 import random
 import sys
+
 
 def get_answers(data):
     deep_answers = [get_answers_to_qset(qset) for qset in data]
     flat_answers = [item for sublist in deep_answers for item in sublist]
     return flat_answers
 
+
 def get_answers_to_qset(qset):
     return [[random.choice(q['candidates'])] for q in qset['questions']]
+
 
 # Should be a call to an external read code for quebap format
 def read_data(data_filename):
     with open(data_filename) as data_file:
         data = json.load(data_file)
         return data
+
 
 def main():
     random.seed(1)
@@ -23,4 +29,5 @@ def main():
         answers = get_answers(data)
         print(json.dumps(answers, indent=2))
 
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    main()
