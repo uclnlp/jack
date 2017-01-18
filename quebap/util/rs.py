905d080ca99f0ebcbd7ca3398e09c30b404c527f
mbosnjak <matko.bosnjak@gmail.com>
2017-01-18 19:01:23 +0000

    Issue #110, added 'global' RansomState. Needs discussion for usage

---
+++ quebap/util/rs.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def singleton(cls):
+    instances = {}
+
+    def getinstance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return getinstance
+
+
+@singleton
+class DefaultRandomState(np.random.RandomState):
+    def __init__(self, seed=None):
+        super().__init__(seed)
+
+
+rs = DefaultRandomState(1337)
+
