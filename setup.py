d033a647642824bc85a3c9e06430e26832be960e
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-01-19 16:37:15 +0100

    quebap -> jtr into setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='quebap',
+setup(name='jtr',
       version='0.1.0',
-      description='QUEstion answering and knowledge BAse Population',
+      description='Jack the Reader is a Python framework for Machine Reading',
       author='UCL Machine Reading',
       author_email='s.riedel@cs.ucl.ac.uk',
-      url='https://github.com/uclmr/quebap',
+      url='https://github.com/uclmr/jtr',
       test_suite='tests',
       license='MIT',
       install_requires=[
