1a71e894d189b29f7059c1d02f4c1680e488ba47
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-01-20 12:17:02 +0100

    Issue #123 - Adding models ComplEx and ER-MLP (from the Google Knowledge Vault paper)

--- jtr/nn/kbp/base.py
+++ jtr/nn/kbp/base.py
@@ -89,7 +89,70 @@ class BilinearModel(BaseModel):
         return self.similarity_function(sW, self.object_embeddings)
 
 
+class ComplexModel(BaseModel):
+    def __init__(self, embedding_size=None, *args, **kwargs):
+        """
+        Implementation of a compositional extension of the ComplEx model [1]
+        [1] Trouillon, T. et al. - Complex Embeddings for Simple Link Prediction - ICML 2016
+
+        :param embedding size: Embedding size.
+        """
+        super().__init__(*args, **kwargs)
+        self.embedding_size = embedding_size
+
+    def __call__(self):
+        """
+        :return: (batch_size) Tensor containing the scores associated by the models to the walks.
+        """
+        es_re, es_im = self.subject_embeddings[:, :self.embedding_size], self.subject_embeddings[:, self.embedding_size:]
+        eo_re, eo_im = self.object_embeddings[:, :self.embedding_size], self.object_embeddings[:, self.embedding_size:]
+        ew_re, ew_im = self.predicate_embeddings[:, :self.embedding_size], self.predicate_embeddings[:, self.embedding_size:]
+
+        def dot3(arg1, rel, arg2):
+            return tf.reduce_sum(arg1 * rel * arg2, axis=1)
+
+        score = dot3(es_re, ew_re, eo_re) + dot3(es_re, ew_im, eo_im) + dot3(es_im, ew_re, eo_im) - dot3(es_im, ew_im, eo_re)
+        return score
+
+
+class ERMLP(BaseModel):
+    def __init__(self, entity_embedding_size=None, predicate_embedding_size=None,
+                 hidden_size=None, f=tf.tanh, reuse_variables=False, *args, **kwargs):
+        """
+        Implementation of the ER-MLP model described in [1, 2]
+
+        [1] Dong, X. L. et al. - Knowledge Vault: A Web-Scale Approach to Probabilistic Knowledge Fusion - KDD 2014
+        [2] Nickel, M. et al. - A Review of Relational Machine Learning for Knowledge Graphs - IEEE 2016
+        """
+        super().__init__(*args, **kwargs)
+        self.f = f
+
+        self.entity_embedding_size = entity_embedding_size
+        self.predicate_embedding_size = predicate_embedding_size
+        input_size = self.entity_embedding_size + self.entity_embedding_size + self.predicate_embedding_size
+
+        with tf.variable_scope("ERMLP", reuse=reuse_variables) as _:
+            self.C = tf.get_variable('C', shape=[input_size, hidden_size], initializer=tf.contrib.layers.xavier_initializer())
+            self.w = tf.get_variable('w', shape=[hidden_size, 1], initializer=tf.contrib.layers.xavier_initializer())
+
+    def __call__(self):
+        """
+        :return: (batch_size) Tensor containing the scores associated by the models to the walks.
+        """
+        e_ijk = tf.concat(1, [self.subject_embeddings, self.object_embeddings, self.predicate_embeddings])
+        h_ijk = tf.matmul(e_ijk, self.C)
+        f_ijk = tf.squeeze(tf.matmul(self.f(h_ijk), self.w), axis=1)
+
+        return f_ijk
+
+    def get_params(self):
+        params = super(self).get_params() + [self.C, self.w]
+        return params
+
+
 # Aliases
 TransE = TranslatingEmbeddings = TranslatingModel
 DistMult = BilinearDiagonal = BilinearDiagonalModel
 RESCAL = Bilinear = BilinearModel
+ComplEx = ComplexE = ComplexModel
+ER_MLP = ERMLP
