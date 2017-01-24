6b8f579aaafe08e72a03b2aa68bc4cffee1a3b2f
mbosnjak <matko.bosnjak@gmail.com>
2017-01-24 18:39:12 +0000

    Small change in NYT download.sh

--- data/NYT/download.sh
+++ data/NYT/download.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 wget -O data/NYT/naacl2013.txt.zip https://www.dropbox.com/s/5iulumlihydo1k7/naacl2013.txt.zip?dl=1
 unzip data/NYT/naacl2013.txt.zip -d data/NYT/
 rm data/NYT/naacl2013.txt.zip
