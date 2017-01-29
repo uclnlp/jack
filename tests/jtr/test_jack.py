02256c9db0a0f4cdf3fbfaf512f879aec5bb233d
riedelcastro <sebastian.riedel@gmail.com>
2017-01-29 11:14:20 +0000

    jack overfit unit test

---
+++ tests/jtr/test_jack.py
@@ -0,0 +1,25 @@
+import jtr.jack as jack
+import jtr.example as example
+from jtr.data_structures import *
+from jtr.preprocess.vocab import Vocab
+
+
+def test_example_reader_overfit():
+    vocab = jack.SharedVocab(Vocab())
+    input_module = example.ExampleInputModule(vocab)
+    model_module = example.ExampleModelModule(vocab)
+    output_module = example.ExampleOutputModule()
+    reader = jack.Reader(input_module, model_module, output_module, vocab)
+
+    example_input = Input(["the father of Bart is Homer", "the father of Homer is Abe"], "Who is the father of Homer?",
+                          ["Homer", "Bart", "Abe", "Lisa"])
+    train_data = [(example_input, "Abe")]
+
+    dev_data = train_data
+    test_data = train_data
+
+    reader.train(train_data, dev_data, test_data)
+
+    answer = reader([example_input])
+
+    assert answer[0].text == "Abe"
