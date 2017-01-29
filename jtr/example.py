fae724cdb509668ebdb8eaee4688a3d5b5426671
rockt <tim.rocktaeschel@gmail.com>
2017-01-29 11:24:56 +0000

    training generator draft for example input module

--- jtr/example.py
+++ jtr/example.py
@@ -1,6 +1,6 @@
 from jtr.jack import *
 import tensorflow as tf
-from jtr.pipelines import pipeline
+from jtr.pipelines import pipeline, deep_seq_map, deep_map
 from typing import Mapping
 from jtr.preprocess.batch import get_batches
 
@@ -9,41 +9,51 @@ class ExampleInputModule(InputModule):
     def store(self):
         pass
 
-    # fixme: providing the input module with a vocab does not make so much sense
-    # as we first have to go through the corpus to build the vocab, and doing
-    # this depends on the pipeline which is encapsulated within this module
     def __init__(self, vocab=None, config=None):
         self.vocab = vocab
         self.config = config
 
     def training_generator(self, training_set: List[Tuple[Input, Answer]]) \
             -> Iterable[Mapping[TensorPort, np.ndarray]]:
-        corpus = {"support": [], "question": [], "candidates": [], "answer": []}
+        corpus = {"support": [], "question": [], "candidates": [], "answers": []}
         for x, y in training_set:
             corpus["support"].append(x.support)
             corpus["question"].append(x.question)
             corpus["candidates"].append(x.candidates)
-            corpus["answer"].append(y)
-        corpus, vocab, target_vocab, candidate_vocab = pipeline(corpus)
+            corpus["answers"].append(y.text)
+
+        # todo: for now using the same vocab
+        corpus, vocab, _, _ = \
+            pipeline(corpus, self.vocab, sepvocab=False)
+
+        if self.vocab is None:
+            self.vocab = vocab
+
         xy_dict = {
             Ports.multiple_support: corpus["support"],
             Ports.question: corpus["question"],
             Ports.atomic_candidates: corpus["candidates"],
-            Ports.candidate_targets: corpus["answer"]
+            Ports.candidate_targets: corpus["answers"]
         }
-        # fixme: we need to map ids to embeddings at this point already
-        # the problem is that this becomes inefficient as we should perform
-        # this mapping on a per-batch basis
 
-        # solution?
-        generator = get_batches(xy_dict)
-        def embed(x):
-            return x
-        generator.iterator = [embed(x) for x in generator.iterator]
+        return self.embed(get_batches(xy_dict))
 
-        return get_batches(xy_dict)
+    def embed(self, generator):
+        # fixme: hard coded input size should be replaced by config
+        input_size = 10
+        embeddings = tf.get_variable(
+            "embeddings", [len(self.vocab), input_size],
+            trainable=True, dtype="float32")
 
+        keys = self.output_ports + [self.target_port]
 
+        def embed_batch(batch):
+            return deep_map(
+                batch, lambda xs: tf.nn.embedding_lookup(embeddings, xs), keys)
+
+        generator = [embed_batch(x) for x in generator.iterator()]
+
+        return generator
 
     def __call__(self, inputs: List[Input]) -> Mapping[TensorPort, np.ndarray]:
         corpus = {"support": [], "question": [], "candidates": []}
@@ -58,11 +68,14 @@ class ExampleInputModule(InputModule):
             Ports.question: corpus["question"],
             Ports.atomic_candidates: corpus["candidates"]
         }
+
+        # todo
+
         return x_dict
 
     @property
     def output_ports(self) -> List[TensorPort]:
-        return [Ports.multiple_support, Ports.question]
+        return [Ports.multiple_support, Ports.question, Ports.atomic_candidates]
 
     @property
     def target_port(self):
@@ -87,7 +100,7 @@ class ExampleModelModule(SimpleModelModule):
 
     @property
     def input_ports(self) -> List[TensorPort]:
-        return [Ports.multiple_support, Ports.question, Ports.candidate_targets]
+        return [Ports.multiple_support, Ports.question, Ports.atomic_candidates]
 
     @property
     def loss_port(self) -> TensorPort:
@@ -96,19 +109,22 @@ class ExampleModelModule(SimpleModelModule):
     # output scores and loss tensor
     def create(self, target: tf.Tensor, support: tf.Tensor, question: tf.Tensor,
                candidates: tf.Tensor) -> (tf.Tensor, tf.Tensor):
-        with tf.variable_scope("embedders") as varscope:
-            question_embedded = question  # todo: nvocab(question)
-            varscope.reuse_variables()
-            candidates_embedded = candidates  # todo: nvocab(candidates)
+        # with tf.variable_scope("embedders") as varscope:
+        #     question_embedded = question  # todo: nvocab(question)
+        #     varscope.reuse_variables()
+        #     candidates_embedded = candidates  # todo: nvocab(candidates)
+        #
+        # question_encoding = tf.reduce_sum(question_embedded, 1)
+        #
+        # scores = tf.reduce_sum(
+        #     tf.expand_dims(question_encoding, 1) * candidates_embedded, 2)
+        #
+        # loss = tf.reduce_mean(
+        #     tf.nn.softmax_cross_entropy_with_logits(scores, target),
+        #     name='predictor_loss')
 
-        question_encoding = tf.reduce_sum(question_embedded, 1)
-
-        scores = tf.reduce_sum(
-            tf.expand_dims(question_encoding, 1) * candidates_embedded, 2)
-
-        loss = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(scores, target),
-            name='predictor_loss')
+        scores = 0
+        loss = 0
 
         return scores, loss
 
@@ -125,11 +141,15 @@ class ExampleOutputModule(OutputModule):
         return []
 
 
-data_set = [Input(["a is true", "b isn't"], "which is it?", ["a", "b", "c"])]
+data_set = [
+    (Input(["a is true", "b isn't"], "which is it?", ["a", "b", "c"]),
+     Answer("a", 1.0))
+]
 
 example_reader = Reader(ExampleInputModule(),
                         ExampleModelModule(),
                         ExampleOutputModule())
-# example_reader.train(data_set)
 
-answers = example_reader(data_set)
+example_reader.train(data_set)
+
+# answers = example_reader(data_set)
