dab5b7b0fcfd984a406ed17eeb64b6747a921436
riedelcastro <sebastian.riedel@gmail.com>
2017-02-15 23:14:12 +0000

    round-trip conversion finished for keyphrases (relations still need to be done)

--- projects/nerre/data.py
+++ projects/nerre/data.py
@@ -1,11 +1,12 @@
 import os
 from collections import defaultdict
-from typing import NamedTuple, Sequence, Mapping
+from typing import NamedTuple, Sequence, Mapping, Tuple
 
 # load training, dev and test data
 from jtr.preprocess.batch import get_batches
 from jtr.preprocess.map import deep_map
 from jtr.preprocess.vocab import Vocab
+import numpy as np
 
 train_dir = "/Users/riedel/corpora/scienceie/train2"
 dev_dir = "/Users/riedel/corpora/scienceie/dev/"
@@ -17,9 +18,13 @@ Token = NamedTuple("Token", [("token_start", int),
 
 Sentence = NamedTuple("Sentence", [("tokens", Sequence[Token])])
 
-Keyphrase = NamedTuple("KeyPhrase", [("start", int), ("end", int), ("type", str)])
+Keyphrase = NamedTuple("KeyPhrase", [("start", int), ("end", int), ("type", str), ("text", str)])
 
-Instance = NamedTuple("Instance", [("doc", Sequence[Sentence]), ("labels", Mapping[Keyphrase, Token])])
+Instance = NamedTuple("Instance", [("text", str),
+                                   ("doc", Sequence[Sentence]),
+                                   ("labels", Mapping[Keyphrase, Sequence[Token]]),
+                                   ("file_name", str),
+                                   ("relations", Sequence[Tuple[str, Keyphrase, Keyphrase]])])
 
 bio_vocab = Vocab()
 bio_vocab("B")
@@ -36,6 +41,7 @@ label_vocab.freeze()
 rel_type_vocab = Vocab()
 rel_type_vocab("HYPONYM_OF")
 rel_type_vocab("SYNONYM_OF")
+rel_type_vocab("O")
 rel_type_vocab.freeze()
 
 
@@ -98,12 +104,17 @@ def read_ann(textfolder=dev_dir):
 
         # mapping from tokens to their labels
         token_to_labels = defaultdict(set)
-        labels_to_tokens = defaultdict(set)
+        keyphrase_to_tokens = defaultdict(set)
+        relations = []
+        id_to_keyphrase = {}
+
+        resolved_relations = []
 
         for l in f_anno:
             anno_inst = l.strip("\n").split("\t")
             if len(anno_inst) == 3:
                 anno_inst1 = anno_inst[1].split(" ")
+                annotation_id = anno_inst[0].strip()
                 if len(anno_inst1) == 3:
                     keytype, start, end = anno_inst1
                 else:
@@ -113,15 +124,29 @@ def read_ann(textfolder=dev_dir):
                     # look up span in text and print error message if it doesn't match the .ann span text
                     keyphr_text_lookup = text[int(start):int(end)]
                     keyphr_ann = anno_inst[2]
-                    if keyphr_text_lookup != keyphr_ann:
-                        print("Spans don't match for anno " + l.strip() + " in file " + f)
+                    keyphrase = Keyphrase(int(start), int(end), keytype, keyphr_text_lookup)
+                    id_to_keyphrase[annotation_id] = keyphrase
+                    assert keyphr_text_lookup == keyphr_ann
                     for offset in range(int(start), int(end)):
                         token = offset_to_token.get(offset, None)
                         if token:
                             token_to_labels[token].add(keytype)
-                            labels_to_tokens[Keyphrase(int(start), int(end), keytype)].add(token)
-        instances.append(Instance(doc, labels_to_tokens))
-        # print(Instance(doc, labels_to_tokens))
+                            keyphrase_to_tokens[keyphrase].add(token)
+                else:
+                    relations.append((annotation_id, anno_inst1))
+        for annotation_id, (rel, arg1, arg2) in relations:
+            arg1_id = arg1.split(":")[1]
+            arg2_id = arg2.split(":")[1]
+            arg1_kp = id_to_keyphrase[arg1_id]
+            arg2_kp = id_to_keyphrase[arg2_id]
+            resolved_relations.append((rel, arg1_kp, arg2_kp))
+
+        sorted_kp_to_tokens = {}
+        for kp, tokens in keyphrase_to_tokens.items():
+            sorted_kp_to_tokens[kp] = sorted(tokens, key=lambda t: t.token_start)
+
+        instances.append(Instance(text, doc, sorted_kp_to_tokens, f, resolved_relations))
+        # print(Instance(doc, keyphrase_to_tokens))
     print("Collected {} word types".format(len(word_types)))
     return instances
 
@@ -131,20 +156,26 @@ def convert_to_batchable_format(instances, vocab,
                                 document_indices_ph="document_indices",
                                 bio_labels_as_ints_ph="bio_labels_as_ints",
                                 type_labels_as_ints_ph="type_labels_as_ints",
-                                token_char_offsets_ph="token_char_offsets"):
+                                token_char_offsets_ph="token_char_offsets",
+                                relation_matrices_ph="relation_matrices",
+                                sentence_lengths_ph="sentence_length"):
     # convert
     sentences_as_ints = []
     bio_labels_as_ints = []
     type_labels_as_ints = []
     document_indices = []
     token_char_offsets = []
+    relation_matrices = []
+    sentence_lengths = []
     for doc_index, instance in enumerate(instances):
         for sentence in instance.doc:
             sentences_as_ints.append([vocab(token.word) for token in sentence.tokens])
             token_char_offsets.append([[token.token_start, token.token_end] for token in sentence.tokens])
             document_indices.append(doc_index)
+            sentence_lengths.append(len(sentence.tokens))
             bio_labels = [bio_vocab("O")] * len(sentence.tokens)
             type_labels = [label_vocab("O")] * len(sentence.tokens)
+            sentence_tokens = set(sentence.tokens)
 
             for kp, kp_tokens in instance.labels.items():
                 started = False
@@ -159,12 +190,25 @@ def convert_to_batchable_format(instances, vocab,
             bio_labels_as_ints.append(bio_labels)
             type_labels_as_ints.append(type_labels)
 
+            relation_matrix = np.ndarray([len(sentence.tokens), len(sentence.tokens)], dtype=np.int32)
+            relation_matrix.fill(rel_type_vocab("O"))
+
+            for rel, arg1, arg2 in instance.relations:
+                if instance.labels[arg1][0] in sentence_tokens and instance.labels[arg2][0] in sentence_tokens:
+                    tok1 = instance.labels[arg1][0]
+                    tok2 = instance.labels[arg2][0]
+                    relation_matrix[tok1.index, tok2.index] = rel_type_vocab(rel)
+            relation_matrices.append(relation_matrix.tolist())
+
     return {
         sentences_as_ints_ph: sentences_as_ints,
         document_indices_ph: document_indices,
         bio_labels_as_ints_ph: bio_labels_as_ints,
         type_labels_as_ints_ph: type_labels_as_ints,
-        token_char_offsets_ph: token_char_offsets
+        token_char_offsets_ph: token_char_offsets,
+        relation_matrices_ph: relation_matrices,
+        sentence_lengths_ph: sentence_lengths
+
     }
 
 
@@ -175,14 +219,87 @@ def fill_vocab(instances, vocab):
                 vocab(token.word)
 
 
+def convert_batch_to_ann(batch, instances,
+                         sentences_as_ints_key="sentences_as_ints",
+                         document_indices_key="document_indices",
+                         bio_labels_key="bio_labels_as_ints",
+                         type_labels_as_ints_key="type_labels_as_ints",
+                         token_char_offsets_key="token_char_offsets",
+                         sentence_lengths_key="sentence_length"):
+    doc_id_to_doc_info = {}
+    doc_ids = batch[document_indices_key]
+    bio_labels = batch[bio_labels_key]
+    type_labels = batch[type_labels_as_ints_key]
+    sentences = batch[sentences_as_ints_key]
+    token_char_offsets = batch[token_char_offsets_key]
+    sentence_lengths = batch[sentence_lengths_key]
+
+    for elem_index, doc_id in enumerate(doc_ids):
+        instance = instances[doc_id]
+        current_kps = doc_id_to_doc_info.get(instance.file_name, {})
+        current_kp_start = 0
+        current_kp_end = -1
+        in_kp = False
+        last_symbol = "O"
+        kp_type = None
+        sentence_length = sentence_lengths[elem_index]
+        print(instance.file_name)
+        print(instance)
+
+        def create_kp():
+            text = instance.text[current_kp_start:current_kp_end]
+            kp_id = "T" + str(len(current_kps) + 1)
+            kp = Keyphrase(current_kp_start, current_kp_end, kp_type, text)
+            current_kps[kp_id] = kp
+
+        bio_label_sequence = bio_labels[elem_index]
+        type_label_sequence = type_labels[elem_index]
+        token_char_offset_sequence = token_char_offsets[elem_index]
+        for bio_label, type_label, (start, end) in zip(bio_label_sequence[:sentence_length],
+                                                       type_label_sequence[:sentence_length],
+                                                       token_char_offset_sequence[:sentence_length]):
+            bio_label_symbol = bio_vocab.get_sym(bio_label)
+            type_label_symbol = label_vocab.get_sym(type_label)
+
+            if bio_label_symbol == "B":
+                if in_kp:
+                    create_kp()
+                else:
+                    in_kp = True
+                    current_kp_start = start
+                    kp_type = type_label_symbol
+            elif bio_label_symbol == "O":
+                if last_symbol != bio_label_symbol:
+                    create_kp()
+                    in_kp = False
+
+            current_kp_end = end
+            last_symbol = bio_label_symbol
+        if last_symbol != "O":
+            create_kp()
+
+        print(current_kps)
+
+        doc_id_to_doc_info[instance.file_name] = current_kps
+
+    for file_name, keyphrases in doc_id_to_doc_info.items():
+        with open("/tmp/" + file_name, "w") as ann:
+            for key, kp in keyphrases.items():
+                ann.write("{key}\t{label} {start} {end}\t{text}\n".format(key=key,
+                                                                          label=kp.type,
+                                                                          start=kp.start,
+                                                                          end=kp.end,
+                                                                          text=kp.text))
+
+
 if __name__ == "__main__":
     vocab = Vocab()
     instances = read_ann(dev_dir)
     fill_vocab(instances, vocab)
-    batchable = convert_to_batchable_format(instances[:1], vocab)
+    batchable = convert_to_batchable_format(instances[:2], vocab)
     print(batchable)
-    batches = get_batches(batchable)
+    batches = list(get_batches(batchable))[:2]
     for batch in batches:
-        print(batch)
+        print(convert_batch_to_ann(batch, instances))
 
 # print(instances[0].labels)
