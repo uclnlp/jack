d457ac0600a06f0f322edc49d0cf43fd83c56d3c
Thomas Demeester <thomas@thola.be>
2017-05-12 15:43:34 +0200

    started new branch whyrnn

---
+++ projects/whyrnn/whyrnn_baseline.py
@@ -0,0 +1,180 @@
+
+#general
+import argparse
+import sys
+import os
+import tensorflow as tf
+import numpy as np
+from time import time
+import logging
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger(os.path.basename(sys.argv[0]))
+
+#jack
+import jtr.jack.readers as readers
+from jtr.load.embeddings.embeddings import load_embeddings
+from jtr.jack.data_structures import load_labelled_data
+from jtr.preprocess.vocabulary import Vocab
+from jtr.jack.tasks.mcqa.simple_mcqa import SingleSupportFixedClassInputs
+
+def main():
+
+    #input
+    train_file = "data/SNLI/snli_1.0/snli_1.0_train_jtr_v1.json"
+    dev_file = "data/SNLI/snli_1.0/snli_1.0_dev_jtr_v1.json"
+    test_file = "data/SNLI/snli_1.0/snli_1.0_test_jtr_v1.json"
+
+    parser = argparse.ArgumentParser(description='Baseline SNLI model experiments')
+
+    #data files
+    parser.add_argument('--jtr_path', default='.', help='path to jtr base')
+
+    #debug mode
+    parser.add_argument('--debug', action='store_true',
+                        help="Run in debug mode")
+    parser.add_argument('--debug_examples', default=2000, type=int,
+                        help="If in debug mode, how many examples should be used (default 2000)")
+
+    #tensorboard path
+    parser.add_argument('--tensorboard_path', default='./.tb/',
+                        help='Folder for tensorboard logs')
+
+    #config for preprocessing
+    parser.add_argument('--lowercase', action='store_true', help="Lowercase data")
+
+    #config of Vocab
+    parser.add_argument('--vocab_max_size', default=sys.maxsize, type=int)
+    parser.add_argument('--vocab_min_freq', default=1, type=int)
+
+    #config of embeddings
+    parser.add_argument('--pretrain', action='store_true',
+                        help="Use pretrained embeddings, by default the initialisation is random")
+    parser.add_argument('--normalize_embeddings', action='store_true',
+                        help="Normalize (initial) embeddings")
+    parser.add_argument('--init_embeddings', default='uniform', choices=['uniform', 'normal'])
+
+    #config of model architecture
+    parser.add_argument('--hidden_dim', default=100, type=int,
+                        help="Size of the hidden representations, default 100")
+
+
+    #training
+    parser.add_argument('--batch_size', default=256,
+                        type=int, help="Batch size for training data, default 256")
+    parser.add_argument('--eval_batch_size', default=256,
+                        type=int, help="Batch size when eval=True, default 256")
+    parser.add_argument('--learning_rate', default=0.001, type=float, help="Learning rate, default 0.001")
+    parser.add_argument('--l2', default=0.0, type=float, help="L2 regularization weight, default 0.0")
+    parser.add_argument('--clip_value', default=0, type=float,
+                        help="Gradients clipped between [-clip_value, clip_value] (default = 0, no clipping)")
+    parser.add_argument('--dropout', default=0.0, type=float,
+                        help="Dropout probability (default 0.0 for no dropout)")
+    parser.add_argument('--epochs', default=30, type=int, help="Number of train epochs, default 30")
+
+    #misc
+    parser.add_argument('--seed', default=1337, type=int, help='random seed')
+    parser.add_argument('--write_metrics_to', default=None, type=str,
+                        help='Filename to log the metrics of the EvalHooks')
+
+    args = parser.parse_args()
+
+    #read out explicitly
+    jtr_path = args.jtr_path
+    debug, debug_examples = args.debug, args.debug_examples
+    tensorboard_path = args.tensorboard_path
+    lowercase = args.lowercase
+    vocab_max_size, vocab_min_freq = args.vocab_max_size, args.vocab_min_freq
+    pretrain = args.pretrain
+    init_embeddings, normalize_embeddings = args.init_embeddings, args.normalize_embeddings
+    repr_dim_input = 50 if debug else 300
+    hidden_dim = args.hidden_dim
+    batch_size, eval_batch_size, learning_rate = args.batch_size, args.eval_batch_size, args.learning_rate
+    dropout, l2, clip_value = args.dropout, args.l2, args.clip_value
+    epochs = args.epochs
+    write_metrics_to = args.write_metrics_to
+
+    tf.set_random_seed(args.seed)
+    np.random.seed(args.seed)
+
+    #config params needed for JTReader
+    config = {
+        'batch_size': batch_size,
+        'eval_batch_size': eval_batch_size,
+        'learning_rate': learning_rate,
+        'vocab_min_freq': vocab_min_freq,
+        'vocab_max_size': vocab_max_size,
+        'lowercase': lowercase,
+        'repr_dim_input': repr_dim_input,
+        'repr_dim': hidden_dim,
+        'dropout': dropout,
+        'init_embeddings': init_embeddings,
+        'normalize_embeddings': normalize_embeddings,
+    }
+
+    #logging
+    sw = tf.summary.FileWriter(tensorboard_path)
+
+    #load SNLI data
+    splits = [train_file, dev_file, test_file]
+    max_count = debug_examples if debug else None
+    train_set, dev_set, test_set = [load_labelled_data(os.path.join(args.jtr_path, f), max_count) for f in splits]
+    for s, l in zip([train_set, dev_set, test_set],['train', 'dev', 'test']):
+        logger.info('loaded %d %s instances'%(len(s), l))
+
+    #load pre-trained embeddings
+    embeddings = None
+    if pretrain:
+        if debug:
+            emb_file = 'glove.6B.50d.txt'
+            embeddings = load_embeddings(os.path.join(jtr_path, 'jtr', 'data', 'GloVe', emb_file), 'glove')
+        else:
+            emb_file = 'GoogleNews-vectors-negative300.bin.gz'
+            embeddings = load_embeddings(os.path.join(jtr_path, 'jtr', 'data', 'SG_GoogleNews', emb_file), 'word2vec')
+        logger.info('loaded pre-trained embeddings ({})'.format(emb_file))
+
+    #create Vocab object
+    vocab = Vocab(emb=embeddings)
+
+    #filter dev and test tokens which have pre-trained embeddings (to avoid having to load all)
+    if pretrain:
+        dev_tmp = SingleSupportFixedClassInputs.preprocess(dev_set, lowercase=config['lowercase'],
+                                                           test_time=False, add_lengths=False)
+        test_tmp = SingleSupportFixedClassInputs.preprocess(test_set, lowercase=config['lowercase'],
+                                                            test_time=False, add_lengths=False)
+        vocab.add_pretrained_for_testing(dev_tmp['question'], dev_tmp['support'])
+        vocab.add_pretrained_for_testing(test_tmp['question'], test_tmp['support'])
+        logger.debug('loaded %d filtered pretrained symbols into vocab for dev and test data'%len(vocab.symset_pt))
+
+    #create reader
+    reader = readers.readers["snli_reader"](vocab, config)
+
+    #add hooks
+    from jtr.jack.train.hooks import LossHook
+    hooks = [LossHook(reader, iter_interval=50, summary_writer=sw),
+             readers.eval_hooks['snli_reader'](reader, dev_set, iter_interval=100, info='dev',
+                                               summary_writer=sw, write_metrics_to=write_metrics_to),
+             readers.eval_hooks['snli_reader'](reader, test_set, epoch_interval=args.epochs,
+                                               info='test', write_metrics_to=write_metrics_to)]
+    if args.debug:
+        hooks.append(readers.eval_hooks['snli_reader'](reader, train_set, iter_interval=100, info='train',
+                                                       summary_writer=sw, write_metrics_to=write_metrics_to))
+
+    # Here we initialise our optimiser
+    # we choose Adam with standard momentum values
+    optim = tf.train.AdamOptimizer(config['learning_rate'])
+
+
+    t0 = time()
+    reader.train(optim, train_set,
+                 hooks=hooks,
+                 max_epochs=epochs,
+                 l2=l2,
+                 clip=None if abs(clip_value) < 1.e-12 else [-clip_value, clip_value]
+                 )
+    #todo: check device setup in JTReader.train
+    print('training took %.3f hours'%((time()-t0)/3600.))
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ projects/whyrnn/whyrnn_experimenter.py
@@ -0,0 +1,178 @@
+#creates bash commands for experiments with varying hyperparams.
+import os
+import sys
+import itertools
+import logging
+import argparse
+import numpy as np
+
+#JTRPATH = '/Users/tdmeeste/workspace/jtr'
+JTRPATH = '/users/tdmeeste/workspace/jtr'
+
+LOGPATH = os.path.join('.', 'logs')
+TBPATH = os.path.join('.', 'tb')
+
+def cartesian_product(dicts):
+    return (dict(zip(dicts, x)) for x in itertools.product(*dicts.values()))
+
+def summary(configuration):
+    #not mentioning at the moment:
+    # 'vocab_max_size', 'vocab_max_size', 'tensorboard_folder',
+    # 'eval_batch_size', 'seed', 'logfile', 'write_metrics_to', 'jtr_path'
+    keys2mention = ['pretrain', 'lowercase', 'batch_size', 'hidden_dim', 'vocab_min_freq',
+                    'init_embeddings', 'normalize_embeddings',
+                    'learning_rate', 'l2', 'dropout', 'clip_value',
+                    'epochs', 'debug']
+    if 'debug' in keys2mention and configuration['debug']:
+        keys2mention.append('debug_examples')
+    kvs = [(k, configuration[k]) for k in keys2mention if k in configuration]
+    return '_'.join([('%s=%s' % (k, str(v))) for (k, v) in kvs])
+
+def to_logfile(c, path, tag='experiment', ext='log'):
+    outfile = os.path.join(path, "%s.%s.%s" % (tag, summary(c), ext))
+    return outfile
+
+
+def to_cmd(c, tag, log_path, tensorboard_path, which_gpu=None):
+    snli_baseline = os.path.join(JTRPATH, 'projects', 'suppoRTE', 'snli_baseline.py')
+    command = 'python3 {}' \
+                ' --vocab_max_size {}' \
+                ' --vocab_min_freq {}' \
+                ' --init_embeddings {}' \
+                ' --hidden_dim {}' \
+                ' --batch_size {}' \
+                ' --eval_batch_size {}' \
+                ' --learning_rate {}' \
+                ' --l2 {}' \
+                ' --dropout {}' \
+                ' --clip_value {}' \
+                ' --epochs {}' \
+                ' --seed {}' \
+                ''.format(snli_baseline,
+                          c['vocab_max_size'],
+                          c['vocab_min_freq'],
+                          c['init_embeddings'],
+                          c['hidden_dim'],
+                          c['batch_size'],
+                          c['eval_batch_size'],
+                          c['learning_rate'],
+                          c['l2'],
+                          c['dropout'],
+                          c['clip_value'],
+                          c['epochs'],
+                          c['seed']
+                          )
+    if c['debug']:
+        command += ' --debug'
+    if 'debug_examples' in c:
+        command += ' --debug_examples {}'.format(c['debug_examples'])
+    if c['lowercase']:
+        command += ' --lowercase'
+    if c['pretrain']:
+        command += ' --pretrain'
+    if c['normalize_embeddings']:
+        command += ' --normalize_embeddings'
+
+    command += ' --jtr_path {}'.format(JTRPATH)
+    command += ' --tensorboard_path {}'.format(os.path.join(tensorboard_path, summary(c)))
+    command += ' --write_metrics_to {}'.format(to_logfile(c, log_path, tag=tag, ext='metrics'))
+
+    if which_gpu is not None:
+        command = 'CUDA_VISIBLE_DEVICES={} {}'.format(which_gpu, command)
+
+    return command
+
+
+def main():
+
+    # #debug
+    # hyperparam_space = dict(
+    #     debug=[True],
+    #     lowercase=[False],
+    #     pretrain=[True],
+    #     debug_examples=[10000],
+    #     vocab_max_size=[sys.maxsize],
+    #     vocab_min_freq=[0],
+    #     init_embeddings=['normal'],
+    #     normalize_embeddings=[False],
+    #     hidden_dim=[100],
+    #     batch_size=[1024],
+    #     eval_batch_size=[1024],
+    #     learning_rate=[.01],
+    #     l2=[1.e-5],
+    #     dropout=[0.5],
+    #     clip_value=[0.],
+    #     epochs=[50],
+    #     seed=[1337]
+    # )
+
+    #full
+    hyperparam_space = dict(
+        debug=[False],
+        lowercase=[True],
+        pretrain=[True],
+        vocab_max_size=[sys.maxsize],
+        vocab_min_freq=[1],
+        init_embeddings=['uniform'],
+        normalize_embeddings=[True],
+        hidden_dim=[100],
+        batch_size=[512, 1024],
+        eval_batch_size=[1024],
+        learning_rate=[5e-3],#[1.e-3, 5.e-3],
+        l2=[1.e-5],#[1.e-5, 5.e-5],
+        dropout=[.4],#[0.5, 0.4, 0.6],
+        clip_value=[0.],
+        epochs=[100],
+        seed=[1337]
+    )
+
+    parser = argparse.ArgumentParser(description='Baseline SNLI model experiments')
+    parser.add_argument('--tag', default="test", help="tag for the current experiment")
+    parser.add_argument('--gpu', nargs='*', default=[None], type=int,
+                        help='ids of gpus that get a separate run file (e.g. --gpu 0 1 0 1  '
+                             'for launch scripts for 2 machines with each 2 gpus)')
+    parser.add_argument('--log_path', default=LOGPATH, help='path for execution and metrics logs')
+    parser.add_argument('--tensorboard_path', default=TBPATH, help='path for tensorboard logs')
+    args = parser.parse_args()
+    tag = args.tag
+    gpus = list(args.gpu)
+    log_path = args.log_path
+    tensorboard_path = args.tensorboard_path
+
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+    if not os.path.exists(tensorboard_path):
+        os.makedirs(tensorboard_path)
+
+    configs = cartesian_product(hyperparam_space)
+    config_chunks = [list(a) for a in np.array_split(list(configs), len(gpus))]
+
+    #kill running python3 processes
+    #print("ps aux | grep python3 | awk '{print $2}' | xargs kill -9")
+
+    for i, (gpu, config_chunk) in enumerate(zip(gpus, config_chunks)):
+        sh_file = '%s_e%d_gpu%s.sh'%(tag, i, str(gpu)) if gpu is not None else '%s_e%d.sh'%(tag, i)
+        with open(sh_file, 'w') as fID:
+
+            for job_id, cfg in enumerate(config_chunk):
+                log_file = to_logfile(cfg, log_path, tag=tag, ext='log')
+                completed = False
+                if os.path.isfile(log_file):
+                    with open(log_file, 'r', encoding='utf-8', errors='ignore') as f:
+                        content = f.read()
+                        completed = '\ttest' in content
+                if not completed:
+                    line = '{} > {} 2>&1'.format(to_cmd(cfg,
+                                                        tag,
+                                                        log_path,
+                                                        tensorboard_path,
+                                                        which_gpu=gpu), log_file)
+                    fID.write(line+'\n')
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
+
+
+
+
