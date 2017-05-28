1df73fc44d7052a91bb1a020c9a13da353d93d67
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-28 09:37:39 +0200

    Testing the evaluation metrics for KBP (MR, MRR, Hits@k ..)

---
+++ tests/jtr/eval/kbp/test_kbp_eval.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from jtr.eval.kbp.base import compute_ranks, ranking_summary
+
+triple_to_score_map = {
+    ('a', 'p', 'a'): 1,
+    ('a', 'p', 'b'): 2,
+    ('a', 'p', 'c'): 3,
+    ('a', 'p', 'd'): 4
+}
+
+triples = sorted(triple for triple, _ in triple_to_score_map.items())
+entity_set = {s for (s, _, _) in triples} | {o for (_, _, o) in triples}
+
+
+def scoring_function(triples):
+    return [triple_to_score_map.get(triple, 0) for triple in triples]
+
+
+def test_kbp_eval():
+    ranks, ranks_filtered = compute_ranks(scoring_function=scoring_function, triples=triples, entity_set=entity_set)
+    ranks_l, ranks_r = ranks
+    assert ranks_l == [1, 1, 1, 1]
+    assert ranks_r == [4, 3, 2, 1]
