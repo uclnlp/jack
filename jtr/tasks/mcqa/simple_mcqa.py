8f274a02f69647ca84ad831743309ef56ad848a6
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-28 19:19:29 +0200

    DAM, first stub

--- jtr/tasks/mcqa/simple_mcqa.py
+++ jtr/tasks/mcqa/simple_mcqa.py
@@ -231,26 +231,32 @@ class PairOfBiLSTMOverSupportAndQuestionModel(AbstractSingleSupportFixedClassMod
 
 class DecomposableAttentionModel(AbstractSingleSupportFixedClassModel):
     def forward_pass(self, shared_resources,
-                     Q_ids, Q_lengths,
-                     S_ids,  S_lengths,
+                     question, question_length,
+                     support, support_length,
                      num_classes):
         # final states_fw_bw dimensions:
         # [[[batch, output dim], [batch, output_dim]]
-        S_ids = tf.squeeze(S_ids, 1)
-        S_lengths = tf.squeeze(S_lengths, 1)
+        support = tf.squeeze(support, 1)
+        support_length = tf.squeeze(support_length, 1)
 
-        Q_seq = tf.nn.embedding_lookup(self.question_embedding_matrix, Q_ids)
-        S_seq = tf.nn.embedding_lookup(self.support_embedding_matrix, S_ids)
+        question_embedding = tf.nn.embedding_lookup(self.question_embedding_matrix, question)
+        support_embedding = tf.nn.embedding_lookup(self.support_embedding_matrix, support)
 
-        all_states_fw_bw, final_states_fw_bw = rnn.pair_of_bidirectional_LSTMs(
-            Q_seq, Q_lengths, S_seq, S_lengths, shared_resources.config['repr_dim'],
-            drop_keep_prob=1.0 - shared_resources.config['dropout'],
-            conditional_encoding=True)
-        # ->  [batch, 2*output_dim]
-        final_states = tf.concat([final_states_fw_bw[0][1], final_states_fw_bw[1][1]],axis=1)
-        # [batch, 2*output_dim] -> [batch, num_classes]
-        outputs = simple.fully_connected_projection(final_states, num_classes)
-        return outputs
+        model_kwargs = {
+            'sequence1': question_embedding,
+            'sequence1_length': question_length,
+            'sequence2': support_embedding,
+            'sequence2_length': support_length,
+            'representation_size': 200,
+            'dropout_keep_prob': 1.0 - shared_resources.config['dropout'],
+            'use_masking': True,
+            'prepend_null_token': True
+        }
+
+        from jtr.tasks.mcqa.dam.base import FeedForwardDAMP
+        model = FeedForwardDAMP(**model_kwargs)
+        logits = model()
+        return logits
 
 
 class EmptyOutputModule(OutputModule):
