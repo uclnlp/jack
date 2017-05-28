ca0db2a12f546957047ccb084b45dcf9e05040d6
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-28 18:49:34 +0200

    DAM, first stub

--- jtr/tasks/mcqa/simple_mcqa.py
+++ jtr/tasks/mcqa/simple_mcqa.py
@@ -229,6 +229,30 @@ class PairOfBiLSTMOverSupportAndQuestionModel(AbstractSingleSupportFixedClassMod
         return outputs
 
 
+class DecomposableAttentionModel(AbstractSingleSupportFixedClassModel):
+    def forward_pass(self, shared_resources,
+                     Q_ids, Q_lengths,
+                     S_ids,  S_lengths,
+                     num_classes):
+        # final states_fw_bw dimensions:
+        # [[[batch, output dim], [batch, output_dim]]
+        S_ids = tf.squeeze(S_ids, 1)
+        S_lengths = tf.squeeze(S_lengths, 1)
+
+        Q_seq = tf.nn.embedding_lookup(self.question_embedding_matrix, Q_ids)
+        S_seq = tf.nn.embedding_lookup(self.support_embedding_matrix, S_ids)
+
+        all_states_fw_bw, final_states_fw_bw = rnn.pair_of_bidirectional_LSTMs(
+            Q_seq, Q_lengths, S_seq, S_lengths, shared_resources.config['repr_dim'],
+            drop_keep_prob=1.0 - shared_resources.config['dropout'],
+            conditional_encoding=True)
+        # ->  [batch, 2*output_dim]
+        final_states = tf.concat([final_states_fw_bw[0][1], final_states_fw_bw[1][1]],axis=1)
+        # [batch, 2*output_dim] -> [batch, num_classes]
+        outputs = simple.fully_connected_projection(final_states, num_classes)
+        return outputs
+
+
 class EmptyOutputModule(OutputModule):
 
     def __init__(self):
