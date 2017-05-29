5066b1c702b01536091597980ccb14b4b4fd3472
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-29 13:17:50 +0200

    default: "" -> default: None

--- jtr/cli.py
+++ jtr/cli.py
@@ -35,9 +35,9 @@ def get_config():
     parser = argparse.ArgumentParser(description='Train and Evaluate a Machine Reader')
     parser.add_argument('--reader', default='example_reader', choices=["example_reader"], help="Reading model to use")
     parser.add_argument('--predict', default=True, type=bool, help='Prediction/Test mode')
-    parser.add_argument('--train', default="", type=argparse.FileType('r'), help="jtr training file")
-    parser.add_argument('--dev', default="", type=argparse.FileType('r'), help="jtr dev file")
-    parser.add_argument('--test', default="", type=argparse.FileType('r'), help="jtr test file")
-    parser.add_argument('--out', default="", type=argparse.FileType('r'), help="jtr output file")
+    parser.add_argument('--train', default=None, type=argparse.FileType('r'), help="jtr training file")
+    parser.add_argument('--dev', default=None, type=argparse.FileType('r'), help="jtr dev file")
+    parser.add_argument('--test', default=None, type=argparse.FileType('r'), help="jtr test file")
+    parser.add_argument('--out', default=None, type=argparse.FileType('r'), help="jtr output file")
     config = parser.parse_args()
     return config
