892a7523a8fdd4b0ce108a1922567e938678f8d7
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-31 15:59:41 +0200

    more tests for KBP modules

--- tests/jtr/readers/test_kbp.py
+++ tests/jtr/readers/test_kbp.py
@@ -14,10 +14,12 @@ def test_distmult():
         'repr_dim': 10
     }
 
-    shared_resources = SharedVocabAndConfig(None, config)
+    shared_resources = SharedResources(None, config)
     distmult_reader = readers.readers['distmult_reader'](shared_resources)
     distmult_reader.setup_from_data(data)
 
     answers = distmult_reader(questions)
+
+    assert len(answers) == 5000
 
     assert answers, 'KBP reader should produce answers'
