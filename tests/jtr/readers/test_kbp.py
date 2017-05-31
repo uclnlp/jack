f22698f00c13893cdc9927ba3ef21b75e26d94f6
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-31 12:49:29 +0200

    ops - committed an unfinished test by accident

--- tests/jtr/readers/test_kbp.py
+++ tests/jtr/readers/test_kbp.py
@@ -4,4 +4,5 @@ from jtr.data_structures import load_labelled_data
 
 
 def test_kbp():
-    data = load_labelled_data()
+    data = load_labelled_data('tests/test_data/WN18/wn18-snippet.jtr.json')
+    
