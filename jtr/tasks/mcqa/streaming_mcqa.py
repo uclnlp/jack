a6c8171427963ecb98c86ec33af85dd6fda939c5
Tim Dettmers <tim.dettmers@gmail.com>
2017-06-02 12:38:18 +0100

    Put streaming functionality in their own files.

---
+++ jtr/tasks/mcqa/streaming_mcqa.py
@@ -0,0 +1,94 @@
+from jtr.core import *
+from jtr.data_structures import *
+
+from jtr.util.hdf5_processing.pipeline import Pipeline, DatasetStreamer
+from jtr.util.hdf5_processing.processors import AddToVocab, SaveLengthsToState, ConvertTokenToIdx, StreamToHDF5, Tokenizer, NaiveNCharTokenizer
+from jtr.util.hdf5_processing.processors import JsonLoaderProcessors, DictKey2IterableMapper, RemoveLineOnJsonValueCondition, ToLower
+from jtr.util.hdf5_processing.batching import StreamBatcher
+
+class StreamingSingleSupportFixedClassInputs(InputModule):
+    def __init__(self, shared_vocab_config):
+        self.shared_resources = shared_vocab_config
+
+    @property
+    def training_ports(self) -> Iterable[TensorPort]:
+        return [Ports.Target.target_index]
+
+    @property
+    def output_ports(self) -> Iterable[TensorPort]:
+        """Defines the outputs of the InputModule
+
+        1. Word embedding index tensor of questions of mini-batchs
+        2. Word embedding index tensor of support of mini-batchs
+        3. Max timestep length of mini-batches for support tensor
+        4. Max timestep length of mini-batches for question tensor
+        5. Labels
+        """
+        return [Ports.Input.multiple_support,
+                Ports.Input.question, Ports.Input.support_length,
+                Ports.Input.question_length, Ports.Target.target_index, Ports.Input.sample_id]
+
+    def __call__(self, qa_settings: Iterable[QASetting]) \
+            -> Mapping[TensorPort, np.ndarray]:
+        pass
+
+    def setup_from_data(self, data: Iterable[Tuple[QASetting, Iterable[Answer]]], dataset_name, dataset_identifier='train') -> SharedResources:
+        # tokenize and convert to hdf5
+        # 1. Setup pipeline to save lengths and generate vocabulary
+        tokenizer = nltk.tokenize.WordPunctTokenizer()
+        p = Pipeline(dataset_name, delete_all_previous_data=dataset_identifier=='train')
+        print(dataset_identifier)
+        if dataset_identifier == 'train':
+            p.add_sent_processor(ToLower())
+            p.add_sent_processor(Tokenizer(tokenizer.tokenize))
+            p.add_token_processor(AddToVocab())
+            p.execute(data)
+            p.save_vocabs()
+        else:
+            p.load_vocabs()
+
+        # 2. Process the data further to stream it to hdf5
+        p.clear_processors()
+
+        # save the lengths of the data
+        p.add_sent_processor(Tokenizer(tokenizer.tokenize))
+        p.add_post_processor(SaveLengthsToState())
+        p.execute(data)
+
+        # convert to indicies and stream to HDF5
+        p.clear_processors()
+        p.add_sent_processor(ToLower())
+        p.add_sent_processor(Tokenizer(tokenizer.tokenize))
+        p.add_post_processor(ConvertTokenToIdx())
+        p.add_post_processor(StreamToHDF5(dataset_identifier))
+        p.execute(data)
+
+        if dataset_identifier == 'train':
+            self.shared_resources.config['answer_size'] = p.state['vocab']['general'].num_labels
+            self.shared_resources.vocab = p.state['vocab']['general']
+        return self.shared_resources
+
+    def dataset_generator(self, dataset: Iterable[Tuple[QASetting, Iterable[Answer]]],
+                          is_eval: bool, dataset_name, dataset_identifier) -> Iterable[Mapping[TensorPort, np.ndarray]]:
+
+        batch_size = self.shared_resources.config['batch_size']
+        batcher = StreamBatcher(dataset_name, dataset_identifier, batch_size)
+        self.batcher = batcher
+
+        def gen():
+            for str2var in batcher:
+                feed_dict = {
+                    Ports.Input.multiple_support: str2var['support'].reshape(batch_size, 1, -1),
+                    Ports.Input.question: str2var["input"],
+                    Ports.Target.target_index:  str2var["target"],
+                    Ports.Input.question_length : str2var['input_length'],
+                    Ports.Input.support_length : str2var['support_length'].reshape(batch_size, 1),
+                    Ports.Input.sample_id : str2var['index']
+                }
+
+                yield feed_dict
+
+        return GeneratorWithRestart(gen)
+
+    def setup(self):
+        pass
