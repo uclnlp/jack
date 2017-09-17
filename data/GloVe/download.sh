d1d40230d8ef3870ae528f78d8cdc757bf3c6d7e
diwe01 <diwe01@berlin-161.b.dfki.de>
2017-09-17 20:33:59 +0200

    added fastqa.yaml; update readme; also download glove-840B

--- data/GloVe/download.sh
+++ data/GloVe/download.sh
@@ -12,5 +12,5 @@ else
 fi
 #wget http://nlp.stanford.edu/data/glove.42B.300d.zip
 #unzip glove.42B.300d.zip
-#wget http://nlp.stanford.edu/data/glove.840B.300d.zip
-#unzip glove.840B.300d.zip
+wget http://nlp.stanford.edu/data/glove.840B.300d.zip
+unzip glove.840B.300d.zip
