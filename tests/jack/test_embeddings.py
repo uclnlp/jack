6611dc43033f488b1740c805e6d823675fc63669
bloomsbury <sebastian@bloomsbury.ai>
2017-10-24 14:56:22 +0100

    fix in test (legacy code was still tested)

--- tests/jack/test_embeddings.py
+++ tests/jack/test_embeddings.py
@@ -2,22 +2,6 @@ from jack.io.embeddings import load_embeddings
 import numpy as np
 
 
-def test_memory_maps():
-    import tempfile
-    from jack.io.embeddings.memory_map import save_as_memory_map, load_memory_map
-    embeddings_file = "data/GloVe/glove.the.50d.txt"
-    embeddings = load_embeddings(embeddings_file, 'glove')
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        prefix = tmp_dir + "_memmap_emb"
-        save_as_memory_map(prefix, embeddings)
-        loaded_embeddings = load_memory_map(prefix)
-        assert loaded_embeddings.shape == embeddings.shape
-        assert len(loaded_embeddings.vocabulary) == 1
-        assert loaded_embeddings.vocabulary["the"] == 0
-        assert "foo" not in loaded_embeddings.vocabulary
-        assert np.isclose(loaded_embeddings.get("the"), embeddings.get("the"), 1.e-5).all()
-
-
 def test_memory_map_dir():
     import tempfile
     from jack.io.embeddings.memory_map import save_as_memory_map_dir, load_memory_map_dir
