e13aa75e1e97b7ad441026f0a69de3cb983d398c
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-10-24 21:15:20 +0200

    updating download.sh in GloVe; creating dam.yaml for generating a SOTA model

--- data/GloVe/download.sh
+++ data/GloVe/download.sh
@@ -7,4 +7,7 @@ else
     echo "Downloading glove.840B.300d.txt!"
     wget -P data/GloVe/ http://nlp.stanford.edu/data/glove.840B.300d.zip
     unzip data/GloVe/glove.840B.300d.zip
+    echo "Downloading glove.840B.300d.memory_map_dir!"
+    wget -P data/GloVe/ http://data.neuralnoise.com/jack/embeddings/glove.840B.300d.memory_map_dir.tar.gz
+    tat xvfz data/GloVe/glove.840B.300d.memory_map_dir.tar.gz -C data/GloVe/
 fi
