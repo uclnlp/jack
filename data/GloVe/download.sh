ac18eae94f9b44b3e34ed0ccdf3a4f5c1ad1deaa
Pasquale Minervini <p.minervini@gmail.com>
2017-10-24 21:43:24 +0200

    small improvement in the download script

--- data/GloVe/download.sh
+++ data/GloVe/download.sh
@@ -5,9 +5,9 @@ then
     echo "data/GloVe/glove.840B.300d.txt already exists! Doing nothing!"
 else
     echo "Downloading glove.840B.300d.txt!"
-    wget -P data/GloVe/ http://nlp.stanford.edu/data/glove.840B.300d.zip
+    wget -c -P data/GloVe/ http://nlp.stanford.edu/data/glove.840B.300d.zip
     unzip data/GloVe/glove.840B.300d.zip
     echo "Downloading glove.840B.300d.memory_map_dir!"
-    wget -P data/GloVe/ http://data.neuralnoise.com/jack/embeddings/glove.840B.300d.memory_map_dir.tar.gz
+    wget -c -P data/GloVe/ http://data.neuralnoise.com/jack/embeddings/glove.840B.300d.memory_map_dir.tar.gz
     tar xvfz data/GloVe/glove.840B.300d.memory_map_dir.tar.gz -C data/GloVe/
 fi
