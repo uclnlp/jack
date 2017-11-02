8b2a366b40e3a1a65384f780e110005edad284e8
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-11-02 17:26:36 +0100

    towards addressing 323

--- setup.py
+++ setup.py
@@ -61,8 +61,13 @@ setup(name='jack',
       classifiers=[
           'Development Status :: 4 - Beta',
           'Intended Audience :: Developers',
+          'Intended Audience :: Education',
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
           'Topic :: Scientific/Engineering :: Artificial Intelligence',
       ],
       keywords='tensorflow machine learning natural language processing question answering')
