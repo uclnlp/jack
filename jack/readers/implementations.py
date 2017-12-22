ea9dfcd899857ee5f4a1ad50efe1b44c6a81cdc4
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-12-22 12:24:05 +0000

    towards fixing Issue #351

--- jack/readers/implementations.py
+++ jack/readers/implementations.py
@@ -40,13 +40,13 @@ def kbp_reader(f):
     return f
 
 
-def reader_from_file(reader_dir: str, **kwargs):
+def reader_from_file(load_dir: str, **kwargs):
     shared_resources = create_shared_resources()
-    shared_resources.load(os.path.join(reader_dir, "shared_resources"))
+    shared_resources.load(os.path.join(load_dir, "shared_resources"))
     if kwargs:
         shared_resources.config.update(kwargs)
     reader = readers[shared_resources.config["reader"]](shared_resources)
-    reader.load_and_setup_modules(reader_dir)
+    reader.load_and_setup_modules(load_dir)
     return reader
 
 
