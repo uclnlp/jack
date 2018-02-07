f645d51c666c0243a9892e262474f01a7d479f22
Dirk Weissenborn <dirk.weissenborn@gmail.com>
2018-02-07 18:45:30 +0100

    Update download.sh

--- data/GloVe/download.sh
+++ data/GloVe/download.sh
@@ -4,9 +4,9 @@ if [ -f "data/GloVe/glove.840B.300d.txt" ]
 then
     echo "data/GloVe/glove.840B.300d.txt already exists! Doing nothing!"
 else
-    echo "Downloading glove.840B.300d.txt!"
-    wget -c -P data/GloVe/ http://nlp.stanford.edu/data/glove.840B.300d.zip
-    unzip -d data/GloVe/ data/GloVe/glove.840B.300d.zip
+    # echo "Downloading glove.840B.300d.txt!"
+    # wget -c -P data/GloVe/ http://nlp.stanford.edu/data/glove.840B.300d.zip
+    # unzip -d data/GloVe/ data/GloVe/glove.840B.300d.zip
     echo "Downloading glove.840B.300d.memory_map_dir!"
     wget -c -P data/GloVe/ http://data.neuralnoise.com/jack/embeddings/glove.840B.300d.memory_map_dir.tar.gz
     tar xvfz data/GloVe/glove.840B.300d.memory_map_dir.tar.gz -C data/GloVe/
