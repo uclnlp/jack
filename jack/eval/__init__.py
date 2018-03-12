e812ab924da8bbe29cc278ae383a3c36fc61aa9f
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2018-03-12 18:12:38 +0000

    some refactoring

--- jack/eval/__init__.py
+++ jack/eval/__init__.py
@@ -1,18 +1,4 @@
+# -*- coding: utf-8 -*-
+
 from jack.eval import extractive_qa, link_prediction, classification
-
-evaluators = {
-    'extractive_qa': extractive_qa.evaluate,
-    'link_prediction': link_prediction.evaluate,
-    'classification': None
-}
-
-
-def evaluate_reader(reader, dataset, batch_size):
-    from jack.readers.implementations import extractive_qa_readers, classification_readers, link_prediction_readers
-    reader_name = reader.shared_resources.config.get('reader')
-    if reader_name in extractive_qa_readers:
-        return extractive_qa.evaluate(reader, dataset, batch_size)
-    elif reader_name in link_prediction_readers:
-        return link_prediction.evaluate(reader, dataset, batch_size)
-    elif reader_name in classification_readers:
-        return classification.evaluate(reader, dataset, batch_size)
+from jack.eval.base import evaluators, evaluate_reader
