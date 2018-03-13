44cefec40201d29ade4fbcb43251e4f2907fd594
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2018-03-13 11:59:18 +0000

    some debugging code

--- jack/debug/base.py
+++ jack/debug/base.py
@@ -7,6 +7,30 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def get_total_trainable_variables():
+    """Calculates and returns the number of trainable parameters in the model."""
+    total_parameters = 0
+    for variable in tf.trainable_variables():
+        shape = variable.get_shape()
+        variable_parameters = 1
+        for dim in shape:
+            variable_parameters *= dim.value
+        total_parameters += variable_parameters
+    return total_parameters
+
+
+def get_total_variables():
+    """Calculates and returns the number of parameters in the model (these can be fixed)."""
+    total_parameters = 0
+    for variable in tf.global_variables():
+        shape = variable.get_shape()
+        variable_parameters = 1
+        for dim in shape:
+            variable_parameters *= dim.value
+        total_parameters += variable_parameters
+    return total_parameters
+
+
 def test_update(feed_dict, train_op):
     session = tf.Session()
     session.run(tf.global_variables_initializer())
