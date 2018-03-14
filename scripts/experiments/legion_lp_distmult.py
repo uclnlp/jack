1c7286dcf2830cc7e04179f80615bfc0100c2a66
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2018-03-14 14:45:50 +0000

    cluster experiments

---
+++ scripts/experiments/legion_lp_distmult.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import itertools
+import os
+import os.path
+
+import sys
+import argparse
+import logging
+
+
+def cartesian_product(dicts):
+    return (dict(zip(dicts, x)) for x in itertools.product(*dicts.values()))
+
+
+def summary(configuration):
+    kvs = sorted([(k, v) for k, v in configuration.items()], key=lambda e: e[0])
+    return '_'.join([('%s=%s' % (k, v)) for (k, v) in kvs])
+
+
+def to_cmd(c, _path=None):
+    command = 'python3 ./bin/pipeline_cardinality.py ' \
+              'with config=conf/lp/distmult.yaml ' \
+              'learning_rate={}' \
+              ''.format(c['lr'])
+    return command
+
+
+def to_logfile(c, path):
+    outfile = "%s/legion_card_v1.%s.log" % (path, summary(c).replace("/", "_"))
+    return outfile
+
+
+def main(argv):
+    def formatter(prog):
+        return argparse.HelpFormatter(prog, max_help_position=100, width=200)
+
+    argparser = argparse.ArgumentParser('Generating experiments for the UCL cluster', formatter_class=formatter)
+    argparser.add_argument('--debug', '-D', action='store_true', help='Debug flag')
+    argparser.add_argument('--path', '-p', action='store', type=str, default=None, help='Path')
+
+    args = argparser.parse_args(argv)
+
+    hyperparameters_space_1 = dict(
+        lr=[0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0]
+    )
+
+    configurations = list(cartesian_product(hyperparameters_space_1))
+
+    path = '/home/ucacmin/Scratch/jack/logs/legion_lp_distmult/'
+
+    # Check that we are on the Legion cluster first
+    if os.path.exists('/home/ucacmin/'):
+        # If the folder that will contain logs does not exist, create it
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    command_lines = set()
+    for cfg in configurations:
+        logfile = to_logfile(cfg, path)
+
+        completed = False
+        if os.path.isfile(logfile):
+            with open(logfile, 'r', encoding='utf-8', errors='ignore') as f:
+                content = f.read()
+                completed = 'Relation specific results' in content
+
+        if not completed:
+            command_line = '{} > {} 2>&1'.format(to_cmd(cfg, _path=args.path), logfile)
+            command_lines |= {command_line}
+
+    # Sort command lines and remove duplicates
+    sorted_command_lines = sorted(command_lines)
+    nb_jobs = len(sorted_command_lines)
+
+    header = """#!/bin/bash -l
+
+#$ -cwd
+#$ -S /bin/bash
+#$ -o /dev/null
+#$ -e /dev/null
+#$ -t 1-{}
+#$ -l mem=8G
+#$ -l h_rt=6:00:00
+
+cd /home/ucacmin/workspace/jack
+
+""".format(nb_jobs)
+
+    print(header)
+
+    for job_id, command_line in enumerate(sorted_command_lines, 1):
+        print('test $SGE_TASK_ID -eq {} && {}'.format(job_id, command_line))
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main(sys.argv[1:])
