bb398dfc60b2b2e36636b0a4a7ced23c0e0398c7
mbosnjak <matko.bosnjak@gmail.com>
2018-03-16 12:47:22 +0000

    Fixed reader_from_file dict error

--- jack/readers/implementations.py
+++ jack/readers/implementations.py
@@ -60,7 +60,7 @@ def reader_from_file(load_dir: str, **kwargs):
     shared_resources.load(os.path.join(load_dir, "shared_resources"))
     if kwargs:
         shared_resources.config.update(kwargs)
-    reader = readers[shared_resources.config["reader"]](shared_resources)
+    reader = readers[shared_resources.config["model"]](shared_resources)
     reader.load_and_setup_modules(load_dir)
     return reader
 
