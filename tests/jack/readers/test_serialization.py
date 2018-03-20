55160e9070b994d4aacf8a8975b4d3df8c4cbd71
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2018-03-20 11:29:21 +0000

    towards solving issue #369

--- tests/jack/readers/test_serialization.py
+++ tests/jack/readers/test_serialization.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from jack.readers.implementations import *
+from jack.io.load import loaders
+
 import tempfile
 
 
@@ -20,13 +22,20 @@ def test_serialization():
     for reader in all_readers:
         vocab, config = {}, {}
 
-        shared_resources = SharedResources(vocab, config)
-        reader_instance = reader(shared_resources)
+        data = None
+        if reader in {distmult_reader, complex_reader, transe_reader}:
+            data = loaders['jack']('tests/test_data/WN18/wn18-snippet.jack.json')
+            config['repr_dim'] = 50
 
-        temp_dir_path = tempfile.mkdtemp()
-        reader_instance.store(temp_dir_path)
+        if data is not None:
+            shared_resources = SharedResources(vocab, config)
+            reader_instance = reader(shared_resources)
+            reader_instance.setup_from_data(data)
 
-        reader.load(temp_dir_path)
+            temp_dir_path = tempfile.mkdtemp()
+            reader_instance.store(temp_dir_path)
+
+            reader.load(temp_dir_path)
 
 
 test_serialization()
