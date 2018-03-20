a7934c509402a7fb86a02bca588c2681482ac2af
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2018-03-20 12:23:02 +0000

    towards solving issue #369

--- tests/jack/readers/test_serialization.py
+++ tests/jack/readers/test_serialization.py
@@ -32,11 +32,12 @@ def test_serialization():
             config['repr_dim'] = 50
         elif reader in {cbilstm_nli_reader, dam_snli_reader}:
             data = loaders['snli']('tests/test_data/SNLI/1000_samples_snli_1.0_train.jsonl')
-            embeddings_file = "data/GloVe/glove.the.50d.txt"
-            embeddings = load_embeddings(embeddings_file, 'glove')
+            embeddings = load_embeddings("data/GloVe/glove.the.50d.txt", 'glove')
             vocab = Vocab(emb=embeddings, init_from_embeddings=True)
             config['repr_dim_input'] = 50
             config['repr_dim'] = 50
+        elif reader in {fastqa_reader}:
+            data = loaders['squad']('tests/test_data/squad/snippet_jtr.json')
 
         if data is not None:
             tf.reset_default_graph()
